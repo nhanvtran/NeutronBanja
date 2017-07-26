cb6293c7e9b002bc7b3654bfa67b6eaffc731df1
Nhan Tran <ntran@mac-127796.dhcp.fnal.gov>
2017-07-26 13:46:28 -0500

    first commit

---
+++ dataAnalysis/plotPulses.py
@@ -0,0 +1,64 @@
+import sys
+import ROOT
+import tdrstyle
+tdrstyle.setTDRStyle()
+ROOT.gStyle.SetPadRightMargin(0.08);
+ROOT.gStyle.SetOptFit(0 );
+ROOT.gStyle.SetPalette(55);
+import math
+from array import array
+import os
+
+
+##---------------------------------------
+def main():
+
+	# analyze these files
+	idir = "~/Dropbox/WorkPublic/data/NeutronBANJA";
+	files = [];
+	files.append(idir + "/" + "20170513_rXXX_polystyrene_bismuth207.root");
+	files.append(idir + "/" + "20170513_rXXX_polystyrene_bismuth207-moreVoltage.root");
+	files.append(idir + "/" + "20170712_r580_950V_dc702_foil-down_bismuth207.root");
+	files.append(idir + "/" + "20170712_r580_dc702_bismuth207.root");
+	files.append(idir + "/" + "20170712_r580_gs20_bismuth207.root");
+	files.append(idir + "/" + "20170712_r580_gs20_foil-down_bismuth207.root");
+	files.append(idir + "/" + "20170712_r580_gs20_foil-up_bismuth207_v2.root");
+	files.append(idir + "/" + "20170712_r580_gs20_foil-up_bismuth207_v3.root");
+	files.append(idir + "/" + "20170712_r580_gs20_foil-up_bismuth207_v4.root");
+	files.append(idir + "/" + "20170712_r580_gs20_foil-up_bismuth207_v5.root");
+	files.append(idir + "/" + "20170712_r580_polystyrene_bismuth207.root");	
+ 
+	for f in files:
+		fin = ROOT.TFile(f);
+		tag = os.path.splitext( os.path.basename(f) )[0];
+		print tag;
+		analyze(fin, tag);
+
+def analyze(fin, tag):
+
+	tin = fin.Get("T");
+	h_pulse = ROOT.TH1F("h_pulse",";pulse; t", 100, -40, 0);
+
+	nent = tin.GetEntries();
+	for i in range(nent):
+	
+		if(nent/100 > 0 and i % (1 * nent/100) == 0):
+			sys.stdout.write("\r[" + "="*int(20*i/nent) + " " + str(round(100.*i/nent,0)) + "% done")
+			sys.stdout.flush()
+
+		tin.GetEntry(i);
+
+		cursum = 0;
+		for it in range(100,450):
+			cursum += tin.c1[it];
+		h_pulse.Fill(cursum);
+
+	ctmp = ROOT.TCanvas("ctmp","ctmp",1000,800);
+	h_pulse.Draw();
+	ctmp.SaveAs("plots/pulse_%s.pdf" % (tag));
+	ctmp.SaveAs("plots/pulse_%s.png" % (tag));
+
+##---------------------------------------
+if __name__ == '__main__':
+		main();
+##---------------------------------------     
